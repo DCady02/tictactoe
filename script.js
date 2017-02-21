37b961061c836cd7ff27662dcf93221b00a7ccd8
DCady02 <DJCady@bluevalleyk12.net>
2017-02-21 12:52:28 -0600

    overhaul

--- script.js
+++ script.js
@@ -1,48 +1,42 @@
-var board = {
-	"box": document.getElementsByTagName("td"),
-	"symbol": "",
-	"turn": "o"
-}
-
-//true = x
-//false = o
-
-
-var blocks = [$("#cell0"), $("#cell1"), $("#cell2"), 
-		      $("#cell3"), $("#cell4"), $("#cell5"), 
-			  $("#cell6"), $("#cell7"), $("#cell8"),];	
-
-
-function play () {
-	$(board.box).click(function (){
-		if (board.symbol === "") {
-			if (board.turn === "x") {
-				board.symbol = "X";
-				turn = false;
-				console.log(turn);
-				
-			} else {
-				board.symbol = "O";
-				turn = true;
-				console.log(turn);
-			}
-		}
-		
-	});
-}
-
-function render () {
-	play();
-
-
-	if (turn = true) {
-			$('#turn').text("It is X's Turn");
-		} else {
-			$('#turn').text("It is O's Turn");
-		}
-
-}
-
 $(document).ready(function() {
-	render();	
+
+//Constants
+	var game = true;
+	var x = 'X';
+	var o = 'O';
+	var blank = '';
+
+//Variables
+
+	var turn = x;
+	var board = [blank, blank, blank
+				 blank, blank, blank,
+				 blank, blank, blank];
+
+ //Write function
+
+ 	function write() {
+
+ 		$('td').click(function() {
+ 			//Making sure that the block that was clicked can only be clicked once
+ 			var id = $(event.target).attr('id');
+ 			var digit = parseInt(id.slice(-1));
+ 			//check to see of the block has been clicked on
+ 			if (board[digit] = blank) {
+ 				board[digit] = turn;
+ 				$(board[digit]).html(toUppercase(turn));
+ 				if (turn = x) {
+ 					turn = o;
+ 				} else if (turn = o) {
+ 					turn = x;
+ 				}
+
+ 			} else {
+ 				alert("That box has already been clicked on!")
+ 			}
+
+ 		}); 
+
+ 	}
+
 });
