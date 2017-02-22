f0216153129577d18a330e148aa9d2e4ec154212
DCady02 <DJCady@bluevalleyk12.net>
2017-02-22 13:12:51 -0600

    Almost finished with project
    All that needs to be done is determine whether the game was a draw.

--- script.js
+++ script.js
@@ -2,41 +2,90 @@ $(document).ready(function() {
 
 //Constants
 	var game = true;
-	var x = 'X';
-	var o = 'O';
+	var X = 'X';
+	var O = 'O';
 	var blank = '';
 
 //Variables
 
-	var turn = x;
-	var board = [blank, blank, blank
+	var turn = X;
+	var board = [blank, blank, blank,
 				 blank, blank, blank,
 				 blank, blank, blank];
 
- //Write function
+ 	function main() {
 
- 	function write() {
-
- 		$('td').click(function() {
+ 		$('td').click(function(event) {
  			//Making sure that the block that was clicked can only be clicked once
  			var id = $(event.target).attr('id');
  			var digit = parseInt(id.slice(-1));
  			//check to see of the block has been clicked on
- 			if (board[digit] = blank) {
+ 			if (board[digit] === blank) {
  				board[digit] = turn;
- 				$(board[digit]).html(toUppercase(turn));
- 				if (turn = x) {
- 					turn = o;
- 				} else if (turn = o) {
- 					turn = x;
+ 				$(board[digit]).html(turn.toUpperCase());
+ 				if (turn === X) {
+ 					turn = O;
+ 				} else if (turn === O) {
+ 					turn = X;
  				}
 
  			} else {
  				alert("That box has already been clicked on!")
  			}
 
+ 			$(event.target).html(turn);
+
  		}); 
 
  	}
 
+ 	function win (side) {
+ 		alert(side + " " + "Wins!");
+ 		location.reload();
+ 	}
+
+ 	function check () {
+ 		$('td').click(function() {
+
+ 			if (board[0] === X && board[1] === X && board[2] === X) {
+ 				win("O");
+ 			} else if (board[3] === X && board[4] === X && board[5] === X)  {
+ 				win("O");
+ 			} else if (board[6] === X && board [7] === X && board[8] === X) {
+ 				win("O")
+ 			} else if (board[0] === X && board[3] === X && board[6] === X)  {
+ 				win("O");
+ 			} else if (board[1] === X && board[4] === X && board[7] === X)  {
+ 				win("O");
+ 			} else if (board[2] === X && board[5] === X && board[8] === X)  {
+ 				win("O");
+ 			} else if (board[2] === X && board[4] === X && board[6] === X)  {
+ 				win("O");
+ 			} else if (board[0] === X && board[4] === X && board[8] === X)  {
+ 				win("O");
+ 			}
+
+ 			if (board[0] === O && board[1] === O && board[2] === O) {
+ 				win("X");
+ 			} else if (board[3] === O && board[4] === O && board[5] === O)  {
+ 				win("X");
+ 			} else if (board[6] === O && board [7] === O && board[8] === O) {
+ 				win("X")
+ 			} else if (board[0] === O && board[3] === O && board[6] === O)  {
+ 				win("X");
+ 			} else if (board[1] === O && board[4] === O && board[7] === O)  {
+ 				win("X");
+ 			} else if (board[2] === O && board[5] === O && board[8] === O)  {
+ 				win("X");
+ 			} else if (board[2] === O && board[4] === O && board[6] === O)  {
+ 				win("X");
+ 			} else if (board[0] === O && board[4] === O && board[8] === O)  {
+ 				win("X");
+ 			}
+
+ 		});
+ 	}
+
+ 	main();
+ 	check();
 });
